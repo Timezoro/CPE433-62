69a753dfe24794bee58aa862ffa9ca3f6611636f
Timezoro <81484071+Timezoro@users.noreply.github.com>
2025-03-09 00:02:54 +0700

    Done

--- Program.cs
+++ Program.cs
@@ -296,7 +296,8 @@ namespace DNWS
                     // Get one, show some info
                     _parent.Log("Client accepted:" + clientSocket.RemoteEndPoint.ToString());
                     HTTPProcessor hp = new HTTPProcessor(clientSocket, _parent);
-                    hp.Process();
+                    TaskInfo ti = new TaskInfo(hp);
+                    ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), ti);
                 }
                 catch (Exception ex)
                 {
